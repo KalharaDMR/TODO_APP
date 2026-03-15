8105782701c9521f39af30b9199eaaa6bd14402e
Rusira54321 <rusiradinujaya57@gmail.com>
2026-03-15 13:50:32 +0530

    Fix some bugsss

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,8 @@ const app = express();
 
 // CORS setup
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://frontend:5173'],
-  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-  credentials: true
+  origin: ['http://localhost:5173'],
+  methods: ['GET','POST','PUT','DELETE','OPTIONS']
 };
 
 app.use(cors(corsOptions));
